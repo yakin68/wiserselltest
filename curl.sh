f5415e4ffa3ffa7667b4967adb8436479231c55a
Yücel AKIN <yakin68@gmail.com>
2024-07-13 14:40:22 +0300

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,3 +1,1 @@
-sudo apt-get update
-sudo apt-get install -y curl
 curl -X POST -H 'Content-type: application/json' --data \'{"text":"Hello, World!"}' https://hooks.slack.com/services/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
