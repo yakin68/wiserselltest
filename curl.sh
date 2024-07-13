e312875ac92a9addd7954b7d4b81eaae89543b67
Yücel AKIN <yakin68@gmail.com>
2024-07-13 15:05:20 +0300

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
+// curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '${jsonPayload}' ${env.SLACK_WEBHOOK_URL}"
