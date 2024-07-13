2943d5c4b137576aed7480008669815fb294559b
Yücel AKIN <yakin68@gmail.com>
2024-07-13 14:43:10 +0300

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,1 +1,2 @@
-curl -X POST -H 'Content-type: application/json' --data \'{"text":"Hello, World!"}' https://hooks.slack.com/services/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
+#!/bin/bash
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
